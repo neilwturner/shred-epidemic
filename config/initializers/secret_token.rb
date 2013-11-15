12a108bbc1c7ae2f5c28dbef3efa5311dc7d2bab
Neil <shreddinglikecrazy@hotmail.com>
2013-11-14 20:50:12 -0600

    First commit of final project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShredEpidemic::Application.config.secret_token = 'bd398ada790bd0bdf3402e5e4f5b4f65759d38eae079d1bc3785ce5e6752360112c323e7a0c7fa4038dc84d7b95f739254ac679259f7b0dcd077af6f95c43604'
